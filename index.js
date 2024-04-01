b2e139bdab9232365c314e190b27a95e4399e2d1
Shinder Lin <shinder.lin@gmail.com>
2024-04-01 21:32:05 +0800

    unit 9

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ const app = express();
 app.use(express.static("public"));
 app.use("/", serveIndex("public", { icons: true }));
 
+app.get("/try-sse", (req, res) => {
+  let id = 30;
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+  });
+  setInterval(function () {
+    let now = new Date();
+    res.write(`id: ${id++}\n`);
+    res.write(`data: ${now.toLocaleString()}\n\n`);
+  }, 2000);
+});
+
 app.listen(web_port, () => {
   console.log(`伺服器啟動於通訊埠：${web_port}`);
 });
